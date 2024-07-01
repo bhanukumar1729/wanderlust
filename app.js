4fc00a55dcd52cebab488d0bf8764f390b99cf19
Bhanu Kumar <bhanukumar.boinidi@gmail.com>
2024-07-01 21:23:17 +0530

    /listings resolved

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ app.use((req,res,next)=>{
 
 app.use("/",listingRoute)
 app.use('/admin/',userRoute)
-app.use('/listings/:id/review',reviewRoute)
+app.use('/:id/review',reviewRoute)
 
 app.use((err, req, res, next) => {
     let { statusCode=500, message='something went wrong' } = err;
